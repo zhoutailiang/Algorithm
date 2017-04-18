db5ec70e215d40fefcad89c5a7e5f516a1bb477e
zhoutailiang <goodzhoutailiang@qq.com>
2017-04-19 07:47:37 +0800

    test multidimensional array

---
+++ src/other/MultiDimensionalArray.java
@@ -0,0 +1,10 @@
+package other;
+
+/**
+ * Created by zhoutailiang on 2017/4/15.
+ */
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+    int [][][][]arr=new int[2][2][2][100];//性别，婚姻，吸烟与否，年龄。
+    }
+}
