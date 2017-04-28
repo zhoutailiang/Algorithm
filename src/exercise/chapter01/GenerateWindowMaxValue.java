818b6daba0b0d42653f2289f1365fd0f6028621b
zhoutailiang <goodzhoutailiang@qq.com>
2017-04-28 08:15:32 +0800

    complete GenerateWindowMaxValue

--- src/exercise/chapter01/GenerateWindowMaxValue.java
+++ src/exercise/chapter01/GenerateWindowMaxValue.java
@@ -1,49 +1,57 @@
 package exercise.chapter01;
 
-import utils.Utils;
-
-import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
+
+import utils.Utils;
 
 /**
  * Created by zhoutailiang on 2017/4/27.
  */
 public class GenerateWindowMaxValue {
     public static void main(String[] args) {
-        ArrayDeque<Integer> deque=new ArrayDeque<>();
-        for (int i = 0; i < 10; i++) {
-            deque.addLast(i);
-        }
-        Utils.println(deque);
+        int[] arr = {4, 3, 5, 4, 3, 3, 6, 7, 8, 9, 3, 6, 8, 1, 2, 1, 1, 1, 1, 1, 1, 7, 9, 90, 43, 654, 21, 54, 76, 43, 76, 98, 1};
+        int windowSize = 1;
+        Utils.println(Arrays.toString(generateWindowMaxValue(arr, windowSize)));
     }
-    private int [] generateWindowMaxValue(int [] arr,int windowSize){
-        int resultSize=arr.length-windowSize+1;
-        int [] result=new int[resultSize];
-        Deque<Integer> deque=new LinkedList<>();
+
+    private static int[] generateWindowMaxValue(int[] arr, int windowSize) {
+        int resultSize = arr.length - windowSize + 1;
+        int[] result = new int[resultSize];
+        int resultIndex = 0;
+        Deque<Integer> deque = new LinkedList<>();
         for (int index = 0; index < arr.length; index++) {
-            if(deque.size()==0){
+            if (deque.size() == 0) {
                 deque.addLast(index);
-            }else {
+                //1.判断是否满足最大窗口
+                if (index >= windowSize - 1) {
+                    result[resultIndex++] = arr[deque.getFirst()];
+                }
+            } else {
                 //1.去掉过时的元素
-                if (deque.peekFirst()==index-windowSize){
+                if (deque.peekFirst() == index - windowSize) {
                     deque.removeFirst();
                 }
                 //2.入队列
-                if (arr[index]<=deque.peekLast()){
-                    deque.addLast(arr[index]);
-                }else {
-                    Iterator<Integer> iterator= deque.descendingIterator();
-                    while (iterator.hasNext()){
-                        Integer next=iterator.next();
-                        if (arr[index]>next){
+                if (arr[index] <= arr[deque.peekLast()]) {
+                    deque.addLast(index);
+                } else {
+                    Iterator<Integer> iterator = deque.descendingIterator();
+                    while (iterator.hasNext()) {
+                        Integer next = iterator.next();
+                        if (arr[index] > next) {
                             iterator.remove();
-                        }else {
+                        } else {
                             break;
                         }
                     }
-                    deque.addLast(arr[index]);
+                    deque.addLast(index);
+                }
+                //3.判断是否满足最大窗口
+                if (index >= windowSize - 1) {
+                    result[resultIndex++] = arr[deque.getFirst()];
                 }
             }
         }
