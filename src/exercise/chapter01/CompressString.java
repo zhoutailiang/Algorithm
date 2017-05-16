61223465f86c5a9662a4b5f5aa847f0b38fd135a
zhoutailiang <goodzhoutailiang@qq.com>
2017-05-16 08:16:56 +0800

    Complete CompressString

---
+++ src/exercise/chapter01/CompressString.java
@@ -0,0 +1,39 @@
+package exercise.chapter01;
+
+import utils.Utils;
+
+/**
+ * Created by zhoutailiang on 2017/5/15.
+ */
+public class CompressString {
+
+    public static void main(String[] args) {
+        String input = "aabbccddeeffggg";
+        Utils.println(compressString(input));
+    }
+
+    // TODO: 2017/5/16 统一i==0和！=0的情况
+    public static String compressString(String str) {
+        char lastChar = str.charAt(0);
+        int lastCharCount = 0;
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == lastChar) {
+                lastCharCount++;
+            } else {
+                builder.append(lastChar);
+                builder.append(lastCharCount);
+                lastChar = c;
+                lastCharCount = 1;
+            }
+        }
+        builder.append(lastChar);
+        builder.append(lastCharCount);
+
+        String compressResult = builder.toString();
+
+        return compressResult.length() >= str.length() ? str : compressResult;
+    }
+}
