1aede42a859d97486ae448ed9eb9d1c739b0d052
zhoutailiang <goodzhoutailiang@qq.com>
2017-06-02 08:17:57 +0800

    some commits

---
+++ src/exercise/chapter01/ClearZero.java
@@ -0,0 +1,57 @@
+package exercise.chapter01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import utils.Utils;
+
+/**
+ * Created by zhoutailiang on 2017/5/22.
+ */
+public class ClearZero {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4},
+                {1, 0, 3, 4},
+                {1, 2, 3, 0},
+                {1, 2, 3, 4},
+                {1, 2, 0, 4},
+                {1, 2, 3, 4}};
+        Utils.printTwoDimensionArray(arr);
+        clearZero(arr);
+        Utils.println("--------");
+        Utils.printTwoDimensionArray(arr);
+    }
+
+    private static void clearZero(int[][] arr) {
+        int row = arr.length;
+        int column = arr[0].length;
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> columnRows = new HashSet<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (arr[i][j] == 0) {
+                    zeroRows.add(i);
+                    columnRows.add(j);
+                }
+            }
+        }
+        if (!zeroRows.isEmpty()) {
+
+            for (int zeroRow : zeroRows) {
+                for (int i = 0; i < column; i++) {
+                    if (arr[zeroRow][i] != 0) {
+                        arr[zeroRow][i] = 0;
+                    }
+                }
+            }
+
+            for (int columnRow : columnRows) {
+                for (int i = 0; i < row; i++) {
+                    if (arr[i][columnRow] != 0) {
+                        arr[i][columnRow] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/exercise/chapter01/RotateWord.java
@@ -0,0 +1,27 @@
+package exercise.chapter01;
+
+import utils.Utils;
+
+/**
+ * Created by zhoutailiang on 2017/5/27.
+ */
+public class RotateWord {
+    public static void main(String[] args) {
+        String str1 = "waterbottle";
+        String str2 = "erbottlewat";
+        Utils.println(isRotateWord(str1, str2));
+    }
+
+    private static boolean isRotateWord(String str1, String str2) {
+        if (str1 != null && str2 != null & str1.length() == str2.length() && str1.length() > 0) {
+            String str1str1 = str1 + str1;
+            return isSubstring(str1str1, str2);
+        }
+        return false;
+    }
+
+    private static boolean isSubstring(String str1, String str2) {
+        return false;
+    }
+
+}
