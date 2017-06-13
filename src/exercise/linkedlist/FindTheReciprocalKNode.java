e287ac4ba4fb344dd37985c6e1f1cf7a566b2775
zhoutailiang <goodzhoutailiang@qq.com>
2017-06-13 08:09:06 +0800

    FindTheReciprocalKNode By Recursion

--- src/exercise/linkedlist/FindTheReciprocalKNode.java
+++ src/exercise/linkedlist/FindTheReciprocalKNode.java
@@ -11,15 +11,19 @@ import utils.Utils;
 public class FindTheReciprocalKNode {
     public static void main(String[] args) {
         ArrayList<Integer> values = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 8; i >= 1; i--) {
             values.add(i);
         }
         Node head = Utils.generateLinkedList(values);
         Utils.println(head);
-        Utils.println(findTheReciprocalKNode(head,19).value);
+        Utils.println(findTheReciprocalKNodeByIteration(head, 5).value);
+        findTheReciprocalKNodeByRecursion1(head, 5);
+        Utils.println(findTheReciprocalKNodeByRecursion2(head, 5, new IntWrapper()).value);
+
     }
 
-    private static Node findTheReciprocalKNode(Node head, int k) {
+
+    private static Node findTheReciprocalKNodeByIteration(Node head, int k) {
         Node first = head;
         Node second = head;
         int currentElement = 1;
@@ -32,5 +36,32 @@ public class FindTheReciprocalKNode {
             second = second.next;
         }
         return second;
+    }
+
+    private static int findTheReciprocalKNodeByRecursion1(Node head, int k) {
+        if (head == null) {
+            return 0;
+        }
+        int i = findTheReciprocalKNodeByRecursion1(head.next, k) + 1;
+        if (i == k) {
+            Utils.println(head.value);
+        }
+        return i;
+    }
+
+    private static Node findTheReciprocalKNodeByRecursion2(Node head, int k, IntWrapper i) {
+        if (head == null) {
+            return null;
+        }
+        Node node = findTheReciprocalKNodeByRecursion2(head.next, k, i);
+        i.value = i.value + 1;
+        if (i.value == k) {
+            return head;
+        }
+        return node;
+    }
+
+    static class IntWrapper {
+        int value = 0;
     }
 }
