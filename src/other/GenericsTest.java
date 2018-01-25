141392a4369f596188a3ae58b300aa6d1c1a16d2
ZhouTailiang <goodzhoutailiang@qq.com>
2018-01-25 16:23:02 +0800

    temp

--- src/other/GenericsTest.java
+++ src/other/GenericsTest.java
@@ -1,12 +1,32 @@
 package other;
 
+import utils.Utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zhoutailiang on 2017/5/4.
  */
-public class GenericsTest {
+public class GenericsTest<T> {
+    public List<T> mList=new ArrayList();
     public static void main(String[] args) {
+        GenericsTest<String> test=new GenericsTest();
+        try {
+            Field field=test.getClass().getField("mList");
+            Type type=field.getGenericType();
+            if (type instanceof ParameterizedType){
+                ParameterizedType parameterizedType= (ParameterizedType) type;
+                Utils.println(parameterizedType.getActualTypeArguments()[0]);
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
+}
+class Son extends GenericsTest<String>{
 
 }
-
-
--- src/other/OtherTest.java
+++ src/other/OtherTest.java
@@ -4,6 +4,55 @@ package other;
  * Created by tailiang on 2017/4/10.
  */
 public class OtherTest {
+
     public static void main(String[] args) {
+        Node head = generateLinkedList();
+
+        if (head != null) {
+            int currentValue = head.value;
+            Node currentNode = head.next;
+            while (currentNode!=null) {
+                if (currentValue==currentNode.next.value) {
+                    currentNode.next = currentNode.next.next;
+                }
+
+            }
+        }
+    }
+
+    public static Node generateLinkedList() {
+        Node head = null;
+        Node currentTail = null;
+        for (int i = 0; i < 20; i++) {
+            if (i == 0 && head == null) {
+                head = new Node();
+                head.value = i;
+                currentTail = head;
+            } else {
+                Node node = new Node();
+                node.value = i;
+                currentTail.next = node;
+                currentTail = node;
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            if (i == 0 && head == null) {
+                head = new Node();
+                head.value = i;
+                currentTail = head;
+            } else {
+                Node node = new Node();
+                node.value = i;
+                currentTail.next = node;
+                currentTail = node;
+            }
+        }
+        return head;
+    }
+
+    static class Node {
+        int value;
+        Node next;
+
     }
 }
